6aea4e35d88b0f69b230730599cb1fd903f7e61a
DanaSnellens <danasnellens@gmail.com>
2023-09-23 10:10:25 +0200

    Initial setup

---
+++ src/Fridge.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Fridge {
+    int temperatuur;
+    String merk;
+    List<Ingredient> ingredient;
+    double volume;
+    boolean light;
+
+
+}
---
+++ src/Ingredient.java
@@ -0,0 +1,20 @@
+import javax.xml.namespace.QName;
+import java.sql.Array;
+import java.util.Date;
+
+public class Ingredient {
+String name;
+double gram;
+int amount;
+Date tht;
+
+    public Ingredient(String name, int amount) {
+        this.name = name;
+        this.amount = amount;
+    }
+
+    public List <Ingredient> listIngredients(Ingredient banaan) {
+        List<Ingredient> ingredients = new Array()
+    }
+
+}
